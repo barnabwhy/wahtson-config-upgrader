7da0330305bdb51b3527efc7b109efbab895c237
Barnaby <barnab.why@gmail.com>
2020-05-23 13:06:08 +0100

    b

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const JSON5 = require('json5')
 const fs = require("mz/fs")
 
 const convert = async (fileIn, fileOut) => {
-    const source = await fs.readFile(fileIn).catch(err => {
-        console.error(`Unable to read file ${fileIn}`)
-    })
+    return new Promise((resolve, reject) => {
+        const source = await fs.readFile(fileIn).catch(err => {
+            reject(`Unable to read file ${fileIn}`)
+        })
 
-    try {
-        const asJSON = toml.parse(source)
+        try {
+            const asJSON = toml.parse(source)
 
-        if(fileOut.split(".json")[1] == "5") {
-            await fs.writeFile(fileOut, JSON5.stringify(asJSON, null, 2))
-        } else {
-            await fs.writeFile(fileOut, JSON.stringify(asJSON, null, 2))
+            if(fileOut.split(".json")[1] == "5") {
+                await fs.writeFile(fileOut, JSON5.stringify(asJSON, null, 2))
+            } else {
+                await fs.writeFile(fileOut, JSON.stringify(asJSON, null, 2))
+            }
+        
+            resolve(`${fileIn} converted to ${fileOut}`)
+        } catch(err) {
+            reject(`Unable to convert ${fileIn} to ${fileOut}`)
         }
-    
-        console.log(`${fileIn} converted to ${fileOut}`)
-    } catch(err) {
-        console.error(`Unable to convert ${fileIn} to ${fileOut}`)
-    }
+    })
 }
 
 module.exports = convert
